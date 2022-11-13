6a1788c27b91405d09f549e31a890c247bdcb7a3
TheVy <ntvy2k@gmail.com>
2022-11-13 23:19:38 +0700

    correct path

--- vaccount/models.py
+++ vaccount/models.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import AbstractUser, UserManager
 from django.utils.translation import gettext_lazy as _
-from django.db import models
 from django.contrib.auth.validators import UnicodeUsernameValidator
 import encryption
 
